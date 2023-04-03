ef6c4e51423f2bce52245df546862f36d6a33558
Ahmet Demir <demirahmet2017@gmail.com>
2023-04-04 01:34:13 +0300

    password changes

--- lib/authentication_service.dart
+++ lib/authentication_service.dart
@@ -2,6 +2,6 @@ class AuthenticationService {
   Future<bool> login(String email, String password) async {
     // Replace this with your actual authentication logic
     await Future.delayed(Duration(seconds: 1));
-    return email == 'ahmed@example.com' && password == 'demir';
+    return email == 'ahmed@example.com' && password == 'demir58';
   }
 }
